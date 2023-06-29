21cf2fe23622cf54c769ebecf7d18364b3842435
vancillo <yaredbane@gmail.com>
2023-06-29 17:20:29 -0600

    V-0.1

--- windbnd/src/App.jsx
+++ windbnd/src/App.jsx
@@ -1,16 +1,26 @@
 import './App.css'
 
-function App() {
+function App () {
 
   return (
     <header>
       <nav>
-        <img src="./public/logo.svg" alt="logo del airbnb" />
+        <img src="./src/assets/logo.svg" alt="logo del airbnb" />
         <ul>
           <li className='search-bar'><input type='text' placeholder='Add Guest' /></li>
         </ul>
       </nav>
+      <HotelRooms />
     </header>
+  )
+}
+
+function HotelRooms () {
+  
+  return (
+    <div className='hotel-rooms'>
+      <h1 style={{ fontFamily: 'system-ui', color: '#333333'}}>Stays in finland</h1>
+    </div>
   )
 }
 
