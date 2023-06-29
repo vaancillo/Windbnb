5334cfff98f7cec571c09a8434b008be51ecfe02
vancillo <yaredbane@gmail.com>
2023-06-29 16:05:29 -0600

    making the project with react

---
+++ windbnd/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <header>
+      <nav>
+        <img src="./public/logo.svg" alt="logo del airbnb" />
+        <ul>
+          <li className='search-bar'><input type='text' placeholder='Add Guest' /></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default App
