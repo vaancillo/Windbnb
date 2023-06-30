9ab311dafb2a5ce9c0d9e6b83f7d6a0a96dce405
vancillo <yaredbane@gmail.com>
2023-06-30 12:28:48 -0600

    UPDATE README.md

--- windbnd/src/components/HotelRooms.jsx
+++ windbnd/src/components/HotelRooms.jsx
@@ -1,4 +1,5 @@
 import '../App.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 function HotelRooms () {
   
@@ -10,12 +11,56 @@ function HotelRooms () {
         <section className='hotels-photos-rooms'>
           <li>
             <ul>
-              <img src='https://cdn.forbes.com.mx/2021/12/Hoteles-de-lujo-tres-1-1024x682.jpg' alt='habitacion de lujo ' />
+            <div className="card" style={{width: '18rem'}}>
+              <img className="card-img-top" src="https://cdn.forbes.com.mx/2021/12/Hoteles-de-lujo-tres-1-1024x682.jpg" alt="Card image cap" />
+                 <div className="card-body">
+                    <h5 className="card-title">Entire apartement. 2 beds</h5>
+                    <p className="card-text">Stylest apartement in the center of the city</p>
+                    <a href="#" className="btn-primary">SUPER HOST</a>
+                  </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+              <img className="card-img-top" src="https://media.ambito.com/p/0c5616e28bf58d0403a76b5cc372f420/adjuntos/239/imagenes/036/681/0036681201/1200x675/smart/2jpg.jpg" alt="Card image cap" />
+                 <div className="card-body">
+                    <h5 className="card-title">Private room</h5>
+                    <p className="card-text">Cozy, peaceful and private room with...</p>
+                  </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+              <img className="card-img-top" src="https://www.infobae.com/new-resizer/mgEpxPqcxG2LQAETGta69SRKSfk=/1200x900/filters:format(webp):quality(85)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/05/20152251/Dorado-Beach-a-Ritz-Carlton-Reserve-3.jpg" alt="Card image cap" />
+                 <div className="card-body">
+                    <h5 className="card-title">Entire house</h5>
+                    <p className="card-text">Modern house in remote arena</p>
+                  </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+              <img className="card-img-top" src="https://i.pinimg.com/originals/da/39/ce/da39ceccbd966c17c74c016e311e1ec2.png" alt="Card image cap" />
+                 <div className="card-body">
+                    <h5 className="card-title">Entire apartement. 2 beds</h5>
+                    <p className="card-text">Stylist room in design district</p>
+                    <a href="#" className="btn-primary">SUPER HOST</a>
+                  </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+              <img className="card-img-top" src="https://uh.gsstatic.es/sfAttachPlugin/2009247.jpg" alt="Card image cap" />
+                 <div className="card-body">
+                    <h5 className="card-title">Private room</h5>
+                    <p className="card-text">Modern apartement close to nature</p>
+                  </div>
+            </div>
+            <div className="card" style={{width: '18rem'}}>
+              <img className="card-img-top" src="https://i0.wp.com/tucochinito.com/wp-content/uploads/2019/09/cuarto-lujo-hotel-1.jpg" alt="Card image cap" />
+                 <div className="card-body">
+                    <h5 className="card-title">Entire house</h5>
+                    <p className="card-text">House in remote area</p>
+                  </div>
+            </div>
+              {/* <img src='https://cdn.forbes.com.mx/2021/12/Hoteles-de-lujo-tres-1-1024x682.jpg' alt='habitacion de lujo ' />
               <img src='https://media.ambito.com/p/0c5616e28bf58d0403a76b5cc372f420/adjuntos/239/imagenes/036/681/0036681201/1200x675/smart/2jpg.jpg' alt='habitacion en paris' />
               <img src='https://www.infobae.com/new-resizer/mgEpxPqcxG2LQAETGta69SRKSfk=/1200x900/filters:format(webp):quality(85)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/05/20152251/Dorado-Beach-a-Ritz-Carlton-Reserve-3.jpg' alt='habitacion' />
               <img src='https://i.pinimg.com/originals/da/39/ce/da39ceccbd966c17c74c016e311e1ec2.png' alt='habitacion nuevo estilo' />
               <img src='https://uh.gsstatic.es/sfAttachPlugin/2009247.jpg' alt='habitaciones del nuevo hotel Gran Lujo de Sant Tomás' />
-              <img src='https://i0.wp.com/tucochinito.com/wp-content/uploads/2019/09/cuarto-lujo-hotel-1.jpg' alt='hotel de lujo' />
+              <img src='https://i0.wp.com/tucochinito.com/wp-content/uploads/2019/09/cuarto-lujo-hotel-1.jpg' alt='hotel de lujo' /> */}
             </ul>
           </li>
         </section>
