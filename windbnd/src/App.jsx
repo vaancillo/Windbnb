3da118a14187dfb53dad1b4116bb9277875db0f4
ClexyT <zombirt67@gmail.com>
2023-06-29 19:02:11 -0600

    V-2

---
+++ windbnd/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import HotelRooms from "./components/HotelRooms"
+
+
+const App = () => {
+  return (
+    <div>
+    <Header></Header>
+    <HotelRooms></HotelRooms>
+    </div>
+  )
+}
+
+export default App
---
+++ windbnd/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+  return (
+    <div>
+    <nav>
+      <img src="./src/assets/logo.svg" alt="logo airbnb" />
+      <ul>
+        <li className='search-bar'><input type='text' placeholder='Add Guest' /></li>
+      </ul>
+    </nav>
+    </div>
+)
+}
+
+export default Header
