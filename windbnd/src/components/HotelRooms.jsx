670403ee3edcfb33673182f9024269327feb656b
vancillo <yaredbane@gmail.com>
2023-06-29 23:07:51 -0600

    V-2.0

--- windbnd/src/components/HotelRooms.jsx
+++ windbnd/src/components/HotelRooms.jsx
@@ -1,8 +1,24 @@
+import '../App.css'
+
 function HotelRooms () {
   
     return (
-      <div className='hotel-rooms'>
-        <h1>Stays in finland</h1>
+      <div>
+        <h1>Stays in finland 
+          <span className='hotel-rooms'>12+ stays</span>
+        </h1>
+        <section className='hotels-photos-rooms'>
+          <li>
+            <ul>
+              <img src='https://cdn.forbes.com.mx/2021/12/Hoteles-de-lujo-tres-1-1024x682.jpg' alt='habitacion de lujo ' />
+              <img src='https://media.ambito.com/p/0c5616e28bf58d0403a76b5cc372f420/adjuntos/239/imagenes/036/681/0036681201/1200x675/smart/2jpg.jpg' alt='habitacion en paris' />
+              <img src='https://www.infobae.com/new-resizer/mgEpxPqcxG2LQAETGta69SRKSfk=/1200x900/filters:format(webp):quality(85)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/05/20152251/Dorado-Beach-a-Ritz-Carlton-Reserve-3.jpg' alt='habitacion' />
+              <img src='https://i.pinimg.com/originals/da/39/ce/da39ceccbd966c17c74c016e311e1ec2.png' alt='habitacion nuevo estilo' />
+              <img src='https://uh.gsstatic.es/sfAttachPlugin/2009247.jpg' alt='habitaciones del nuevo hotel Gran Lujo de Sant Tomás' />
+              <img src='https://i0.wp.com/tucochinito.com/wp-content/uploads/2019/09/cuarto-lujo-hotel-1.jpg' alt='hotel de lujo' />
+            </ul>
+          </li>
+        </section>
       </div>
     )
   }
